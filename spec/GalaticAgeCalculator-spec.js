da381fa4a718307a5a7861c37ea8ddbd873b4625
Lan Dam <ldam77@yahoo.com>
2018-08-03 12:43:01 -0700

    add mercury year and test

--- spec/GalaticAgeCalculator-spec.js
+++ spec/GalaticAgeCalculator-spec.js
@@ -2,12 +2,16 @@ import * as ageCalculator from './../src/GalacticAgeCalculator.js';
 
 describe('Galactic Age Calculator', function() {
   it('should convert age from years to seconds', function() {
-    expect(ageCalculator.convertYearAgeToSeconds(1)).toEqual(31536000);
+    expect(ageCalculator.convertYearsToSeconds(1)).toEqual(31536000);
   });
 
   it('should find the difference between two dates in seconds', function() {
     let date1 = new Date('August 02, 2018');
     let date2 = new Date('August 03, 2018');
     expect(ageCalculator.differenceBetweenTwoDatesInSeconds(date1, date2)).toEqual(86400);
+  });
+
+  it('should return age in Mercury Year', function() {
+    expect(ageCalculator.ageOnMercury(10)).toEqual(10/.24);
   });
 });
--- src/GalacticAgeCalculator.js
+++ src/GalacticAgeCalculator.js
@@ -1,7 +1,11 @@
-export function convertYearAgeToSeconds(year) {
+export function convertYearsToSeconds(year) {
   return year*365*24*60*60;
 }
 
 export function differenceBetweenTwoDatesInSeconds(date1, date2) {
   return Math.abs(date1-date2)/1000;
+}
+
+export function ageOnMercury(year) {
+  return year/.24;
 }
